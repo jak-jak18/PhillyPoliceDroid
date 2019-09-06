141860dca3641bad59ee630414ec750ba79bdf91
Justin Katzwhite <jkatzwhite@gmail.com>
2019-09-05 22:24:29 -0400

    Cleaned up MainPreferenceActivity/Fragment

--- app/src/main/java/furious/viewfragments/preferences/MainPreferenceActivity.java
+++ app/src/main/java/furious/viewfragments/preferences/MainPreferenceActivity.java
@@ -9,29 +9,21 @@ import furious.viewfragments.main.MainStart;
 
 public class MainPreferenceActivity extends Activity {
 
-@Override
- protected void onCreate(Bundle savedInstanceState) {
-  // TODO Auto-generated method stub
-  super.onCreate(savedInstanceState);
-  
-  android.app.FragmentManager mFragmentManager = getFragmentManager();
-  android.app.FragmentTransaction mFragmentTransaction = mFragmentManager
-                          .beginTransaction();
-  MainPreferenceFragment mPrefsFragment = new MainPreferenceFragment();
-  mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
-  mFragmentTransaction.commit();
-  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(android.R.id.content, new MainPreferenceFragment())
+                .commit();
+    }
 
- }
- 
- @Override
- public void onBackPressed(){
-   super.onBackPressed();
-   	Intent intent = new Intent(MainPreferenceActivity.this, MainStart.class);
-	intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	startActivity(intent);
-	finish();
- }
- 
-
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        Intent intent = new Intent(MainPreferenceActivity.this, MainStart.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        finish();
+    }
 }
